4ce03c8165b07ef14cd0e0859ccc2a45b02b80b3
Moses <moses.m917@gmail.com>
2025-07-08 10:48:14 +0200

    Initial commit

---
+++ src/main/java/com/mouzeecode/E_com_App/EComAppApplication.java
@@ -0,0 +1,13 @@
+package com.mouzeecode.E_com_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EComAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EComAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mouzeecode/E_com_App/EComAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mouzeecode.E_com_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EComAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
