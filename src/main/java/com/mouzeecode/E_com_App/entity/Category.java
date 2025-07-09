bd76b42ce1fa101e7ce71017e45d8109f20f6fe2
Moses <moses.m917@gmail.com>
2025-07-09 10:02:07 +0200

    Entities updates

---
+++ src/main/java/com/mouzeecode/E_com_App/entity/Category.java
@@ -0,0 +1,19 @@
+package com.mouzeecode.E_com_App.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Set;
+
+@Entity
+@Data
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Set<Product> product;
+}
